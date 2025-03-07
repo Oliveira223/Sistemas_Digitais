73654bdf6f35e2e22b054ee5fc7aed1bb2b1112f
Oliveira223 <162735266+Oliveira223@users.noreply.github.com>
2025-03-07 12:24:12 +0000

    a

--- mux_tb.vhd
+++ mux_tb.vhd
@@ -1,40 +1,34 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.all;
 
 entity mux_tb is
 end mux_tb;
 
 architecture mux_tb_x of mux_tb is
     
-    component somador
-    port(
-        a: in std_logic;
-        b: in std_logic;
-        sel: in std_logic;
-        o: out std_logic
-    );
-
-    end component;
-
+ 
     signal a: std_logic := '0';
     signal b: std_logic := '0';
-    signal sel: std_logic := '0';
-
+    signal sel: std_logic;
+    signal o: std_logic;
     begin 
 
-    dut: mux_2_1_1 port map(
+    dut: entity work.mux port map(
         a => a, 
         b => b,
-        sel => sel
+        sel => sel,
+        o => o
     );
 
     teste: process
     begin
-        wait for 100ns
+        wait for 100ns;
         a<='1';
         b<='1';
+        sel <= '0';
         wait for 10ns;
-    end process;
-
-
+        assert sel ='0' and o = '1' 
+            report "1 + 1 não é 2!" severity error;
+ end process;
 end architecture;
